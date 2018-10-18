cf16f92772ecbdf8e1fad4a239c85227ebc1e263
rjkz808 <38258624+rjkz808@users.noreply.github.com>
2018-10-18 11:28:31 +0300

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const readline = require('readline-sync');
 
-const apiKey = 'v3/ea555dd56ce44c4ebb025fef5698abdd';
+const apiKey = 'ea555dd56ce44c4ebb025fef5698abdd';
 const mnemonic = readline.question('Enter your 12 word mnemonic\n');
 
 module.exports = {
@@ -9,19 +9,19 @@ module.exports = {
     live: this.kovan,
     development: this.ganachecli,
     main: {
-      provider: () => new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/' + apiKey),
+      provider: () => new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/v3/' + apiKey),
       network_id: '1'
     },
     ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/' + apiKey),
+      provider: () => new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/' + apiKey),
       network_id: '3'
     },
     rinkeby: {
-      provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/' + apiKey),
+      provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/' + apiKey),
       network_id: '4'
     },
     kovan: {
-      provider: () => new HDWalletProvider(mnemonic, 'https://kovan.infura.io/' + apiKey),
+      provider: () => new HDWalletProvider(mnemonic, 'https://kovan.infura.io/v3/' + apiKey),
       network_id: '42'
     },
     ganache: {
